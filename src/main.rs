d82f946d76c2216577340fa5ea8fa47ef79aba4b
Juan Milkah <juanmilkah@gmail.com>
2025-03-11 08:07:09 +0300

    parse torrent

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,18 @@ enum BencodeValue<'a> {
     Dictionary(BDictionary<'a>),
 }
 
-/// Strings are length-prefixed base ten followed by a colon and the string. For example 4:spam corresponds to 'spam'.
+/// Strings are length-prefixed base ten followed by a colon and the string.
+/// For example 4:spam corresponds to 'spam'.
 #[derive(Hash, Eq, PartialEq, Debug, Clone)]
 struct BString<'a> {
     content: &'a [u8],
 }
 
-/// Integers are represented by an 'i' followed by the number in base 10 followed by an 'e'. For example i3e corresponds to 3 and i-3e corresponds to -3. Integers have no size limitation. i-0e is invalid. All encodings with a leading zero, such as i03e, are invalid, other than i0e, which of course corresponds to 0.
+/// Integers are represented by an 'i' followed by the number in base 10 followed by an 'e'.
+/// For example i3e corresponds to 3 and i-3e corresponds to -3.
+/// Integers have no size limitation. i-0e is invalid.
+/// All encodings with a leading zero, such as i03e, are invalid,
+/// other than i0e, which of course corresponds to 0.
 #[derive(PartialEq, Debug, Clone)]
 struct BInteger {
     value: i64,
@@ -27,46 +32,169 @@ struct BList<'a> {
     items: Vec<BencodeValue<'a>>,
 }
 
-///Dictionaries are encoded as a 'd' followed by a list of alternating keys and their corresponding values followed by an 'e'. For example, d3:cow3:moo4:spam4:eggse corresponds to {'cow': 'moo', 'spam': 'eggs'} and d4:spaml1:a1:bee corresponds to {'spam': ['a', 'b']}. Keys must be strings and appear in sorted order (sorted as raw strings, not alphanumerics).
+///Dictionaries are encoded as a 'd' followed by a list of alternating keys
+///and their corresponding values followed by an 'e'.
+///For example, d3:cow3:moo4:spam4:eggse corresponds to {'cow': 'moo', 'spam': 'eggs'}
+///and d4:spaml1:a1:bee corresponds to {'spam': ['a', 'b']}.
+///Keys must be strings and appear in sorted order (sorted as raw strings, not alphanumerics).
 #[derive(PartialEq, Debug, Clone)]
 struct BDictionary<'a> {
     dict: HashMap<BString<'a>, BencodeValue<'a>>,
 }
 
-/// metainfo files
 /// Metainfo files (also known as .torrent files)
+#[derive(Debug, PartialEq)]
 struct Torrent {
     /// The URL of the tracker.
     announce: String,
+    /// The metadata
     info: Info,
 }
 
+#[derive(Debug, PartialEq)]
 struct Info {
     ///In the single file case, the name key is the name of a file,
     ///in the muliple file case, it's the name of a directory.
     name: String,
 
     /// piece length maps to the number of bytes in each piece the file is split into.
-    /// For the purposes of transfer, files are split into fixed-size pieces which are all the same length except for possibly the last one which may be truncated. piece length is almost always a power of two, most commonly 2 18 = 256 K (BitTorrent prior to version 3.2 uses 2 20 = 1 M as default).
+    /// For the purposes of transfer,
+    /// files are split into fixed-size pieces which are all the same length
+    /// except for possibly the last one which may be truncated.
+    /// piece length is almost always a power of two,
+    /// most commonly 2 18 = 256 K (BitTorrent prior to version 3.2 uses 2 20 = 1 M as default).
     piece_length: usize,
 
-    /// pieces maps to a string whose length is a multiple of 20. It is to be subdivided into strings of length 20, each of which is the SHA1 hash of the piece at the corresponding index.
+    /// pieces maps to a string whose length is a multiple of 20.
+    /// It is to be subdivided into strings of length 20,
+    /// each of which is the SHA1 hash of the piece at the corresponding index.
     pieces: Vec<u8>,
 
-    /// There is also a key length or a key files, but not both or neither.
-    /// If length is present then the download represents a single file, otherwise it represents a set of files which go in a directory structure.
     /// In the single file case, length maps to the length of the file in bytes.
     length: Option<usize>,
 
-    files: Option<FilesInfo>,
+    ///In the multi file case, a set of files which go in a directory structure.
+    files: Option<Vec<FilesInfo>>,
 }
 
+#[derive(Debug, PartialEq, Clone)]
 struct FilesInfo {
     /// The length of the file, in bytes.
     length: usize,
 
-    /// A list of UTF-8 encoded strings corresponding to subdirectory names, the last of which is the actual file name (a zero length list is an error case).
+    /// A list of UTF-8 encoded strings corresponding to subdirectory names,
+    /// the last of which is the actual file name (a zero length list is an error case).
     path: Vec<String>,
+}
+
+fn parse_torrent(data: &[u8]) -> Result<Torrent, String> {
+    match parse_bencode(data)? {
+        (rest, BencodeValue::Dictionary(dict)) => {
+            if !rest.is_empty() {
+                return Err("Trailing bytes after root element".to_string());
+            }
+
+            let announce = match dict.dict.get(&BString {
+                content: b"announce",
+            }) {
+                Some(BencodeValue::String(BString { content })) => {
+                    String::from_utf8(content.to_vec())
+                }
+                _ => return Err("Invalid or Missing announce url".to_string()),
+            }
+            .unwrap();
+
+            let info = match dict.dict.get(&BString { content: b"info" }) {
+                Some(BencodeValue::Dictionary(dict)) => parse_torrent_info(dict)?,
+                _ => return Err("Invalid or Missing torrent info field".to_string()),
+            };
+
+            Ok(Torrent { announce, info })
+        }
+        _ => Err("Expected root element to be a BDictionary".to_string()),
+    }
+}
+
+fn parse_torrent_info(info: &BDictionary) -> Result<Info, String> {
+    let name = match info.dict.get(&BString { content: b"name" }) {
+        Some(BencodeValue::String(BString { content })) => String::from_utf8(content.to_vec())
+            .map_err(|_| "Invalid utf-8 bytes in name".to_string())?,
+        _ => return Err("Invalid or Missing info name".to_string()),
+    };
+
+    let piece_length = match info.dict.get(&BString {
+        content: b"piece length",
+    }) {
+        Some(BencodeValue::Integer(BInteger { value })) => *value as usize,
+        _ => return Err("Invalid or Missing info piece length".to_string()),
+    };
+
+    // get pieces
+    let pieces = match info.dict.get(&BString { content: b"pieces" }) {
+        Some(BencodeValue::String(BString { content })) => content.to_vec(),
+        _ => return Err("Invalid or Missing pieces info".to_string()),
+    };
+
+    //get length
+    let length = match info.dict.get(&BString { content: b"length" }) {
+        Some(BencodeValue::Integer(BInteger { value })) => Some(*value as usize),
+        None => None,
+        _ => return Err("Invalid info length".to_string()),
+    };
+
+    // files exists only if length does not
+    let files = if length.is_none() {
+        let mut result = Vec::new();
+
+        match info.dict.get(&BString { content: b"files" }) {
+            Some(BencodeValue::List(BList { items })) => {
+                for fileinfo in items {
+                    let file_info = match fileinfo {
+                        BencodeValue::Dictionary(BDictionary { dict }) => dict,
+                        _ => return Err("fileinfo should be a BDictionary".to_string()),
+                    };
+
+                    //get length
+                    let length = match file_info.get(&BString { content: b"length" }) {
+                        Some(BencodeValue::Integer(BInteger { value })) => *value as usize,
+                        _ => return Err("Invalid or MIssing fileinfo length".to_string()),
+                    };
+
+                    //get path
+                    let mut path = Vec::new();
+                    match file_info.get(&BString { content: b"path" }) {
+                        Some(BencodeValue::List(BList { items })) => {
+                            for item in items {
+                                match item {
+                                    BencodeValue::String(BString { content }) => {
+                                        path.push(String::from_utf8(content.to_vec()).map_err(
+                                            |_| "Invalid utf-8 bytes in path".to_string(),
+                                        )?)
+                                    }
+                                    _ => return Err("path item must be a string".to_string()),
+                                }
+                            }
+                        }
+                        _ => return Err("Invalid or Missing fileinfo path".to_string()),
+                    };
+
+                    result.push(FilesInfo { length, path })
+                }
+            }
+            _ => return Err("Missing or Invalid info files".to_string()),
+        }
+        Some(result)
+    } else {
+        None
+    };
+
+    Ok(Info {
+        name,
+        piece_length,
+        pieces,
+        length,
+        files,
+    })
 }
 
 fn parse_bencode(data: &[u8]) -> Result<(&[u8], BencodeValue), String> {
@@ -171,122 +299,174 @@ fn parse_bencode_dictionary(data: &[u8]) -> Result<(&[u8], BencodeValue), String
     ))
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), String> {
+    let torrent = b"d8:announce11:example.com4:infod4:name9:blindspot12:piece lengthi20e6:pieces5:hello5:filesld6:lengthi10e4:pathl5:path15:path2eeeee";
+    let torrent = parse_torrent(torrent)?;
+    println!("{:?}", torrent);
+    Ok(())
 }
 
 #[cfg(test)]
-mod bencoding {
-    use crate::*;
+mod test {
+    mod bencoding {
+        use crate::*;
 
-    #[test]
-    fn test_integer() {
-        // 56
-        let torrent = b"i56e";
+        #[test]
+        fn integer() {
+            // 56
+            let torrent = b"i56e";
 
-        let (rest, value) = parse_bencode(torrent).unwrap();
-        assert_eq!(value, BencodeValue::Integer(BInteger { value: 56 }));
-        assert!(rest.is_empty());
-    }
+            let (rest, value) = parse_bencode(torrent).unwrap();
+            assert_eq!(value, BencodeValue::Integer(BInteger { value: 56 }));
+            assert!(rest.is_empty());
+        }
 
-    #[test]
-    fn test_string() {
-        // "foo"
-        let torrent = b"3:foo";
+        #[test]
+        fn string() {
+            // "foo"
+            let torrent = b"3:foo";
 
-        let (rest, value) = parse_bencode(torrent).unwrap();
-        assert!(rest.is_empty());
-        assert_eq!(value, BencodeValue::String(BString { content: b"foo" }));
-    }
+            let (rest, value) = parse_bencode(torrent).unwrap();
+            assert!(rest.is_empty());
+            assert_eq!(value, BencodeValue::String(BString { content: b"foo" }));
+        }
 
-    #[test]
-    fn test_list() {
-        // ["foo", "bar"]
-        let torrent = b"l3:foo3:bare";
+        #[test]
+        fn list() {
+            // ["foo", "bar"]
+            let torrent = b"l3:foo3:bare";
 
-        let (rest, value) = parse_bencode(torrent).unwrap();
-        assert!(rest.is_empty());
-        assert_eq!(
-            value,
-            BencodeValue::List(BList {
-                items: [
-                    BencodeValue::String(BString { content: b"foo" }),
-                    BencodeValue::String(BString { content: b"bar" })
-                ]
-                .to_vec()
-            })
-        )
-    }
+            let (rest, value) = parse_bencode(torrent).unwrap();
+            assert!(rest.is_empty());
+            assert_eq!(
+                value,
+                BencodeValue::List(BList {
+                    items: [
+                        BencodeValue::String(BString { content: b"foo" }),
+                        BencodeValue::String(BString { content: b"bar" })
+                    ]
+                    .to_vec()
+                })
+            )
+        }
 
-    #[test]
-    fn test_simple_dictionary() {
-        // {"a": "foo"}
-        let torrent = b"d1:a3:fooe";
+        #[test]
+        fn simple_dictionary() {
+            // {"a": "foo"}
+            let torrent = b"d1:a3:fooe";
 
-        let (rest, value) = parse_bencode(torrent).unwrap();
-        assert!(rest.is_empty());
-        assert_eq!(
-            value,
-            BencodeValue::Dictionary(BDictionary {
-                dict: HashMap::from([(
-                    BString { content: b"a" },
-                    BencodeValue::String(BString { content: b"foo" }),
-                ),]),
-            }),
-        )
-    }
-
-    #[test]
-    fn test_simple_dictionary_2() {
-        // {"a": "foo", "mike": "angela"}
-        let torrent = b"d1:a3:foo4:mike6:angelae";
-
-        let (rest, value) = parse_bencode(torrent).unwrap();
-        assert!(rest.is_empty());
-        assert_eq!(
-            value,
-            BencodeValue::Dictionary(BDictionary {
-                dict: HashMap::from([
-                    (
+            let (rest, value) = parse_bencode(torrent).unwrap();
+            assert!(rest.is_empty());
+            assert_eq!(
+                value,
+                BencodeValue::Dictionary(BDictionary {
+                    dict: HashMap::from([(
                         BString { content: b"a" },
                         BencodeValue::String(BString { content: b"foo" }),
-                    ),
-                    (
-                        BString { content: b"mike" },
-                        BencodeValue::String(BString { content: b"angela" })
-                    )
-                ]),
-            }),
-        )
+                    ),]),
+                }),
+            )
+        }
+
+        #[test]
+        fn simple_dictionary_2() {
+            // {"a": "foo", "mike": "angela"}
+            let torrent = b"d1:a3:foo4:mike6:angelae";
+
+            let (rest, value) = parse_bencode(torrent).unwrap();
+            assert!(rest.is_empty());
+            assert_eq!(
+                value,
+                BencodeValue::Dictionary(BDictionary {
+                    dict: HashMap::from([
+                        (
+                            BString { content: b"a" },
+                            BencodeValue::String(BString { content: b"foo" }),
+                        ),
+                        (
+                            BString { content: b"mike" },
+                            BencodeValue::String(BString { content: b"angela" })
+                        )
+                    ]),
+                }),
+            )
+        }
+
+        #[test]
+        fn complex_dictionary() {
+            // {"foo": "bar", "list": ["angela", "james"]}
+            let torrent = b"d3:foo3:bar4:listl6:angela5:jamesee";
+
+            let (rest, map) = parse_bencode(torrent).unwrap();
+            assert!(rest.is_empty());
+            assert_eq!(
+                map,
+                BencodeValue::Dictionary(BDictionary {
+                    dict: HashMap::from([
+                        (
+                            BString { content: b"foo" },
+                            BencodeValue::String(BString { content: b"bar" })
+                        ),
+                        (
+                            BString { content: b"list" },
+                            BencodeValue::List(BList {
+                                items: [
+                                    BencodeValue::String(BString { content: b"angela" }),
+                                    BencodeValue::String(BString { content: b"james" })
+                                ]
+                                .to_vec()
+                            })
+                        )
+                    ])
+                })
+            )
+        }
     }
 
-    #[test]
-    fn test_complex_dictionary() {
-        // {"foo": "bar", "list": ["angela", "james"]}
-        let torrent = b"d3:foo3:bar4:listl6:angela5:jamesee";
+    mod torrent {
+        use crate::*;
+        #[test]
+        fn simple() {
+            let torrent = b"d8:announce11:example.com4:infod4:name9:blindspot12:piece lengthi20e6:pieces5:hello6:lengthi10eee";
+            let torrent = parse_torrent(torrent).unwrap();
+            assert_eq!(
+                torrent,
+                Torrent {
+                    announce: "example.com".to_string(),
+                    info: Info {
+                        name: "blindspot".to_string(),
+                        piece_length: 20,
+                        pieces: [104, 101, 108, 108, 111].to_vec(),
+                        length: Some(10),
+                        files: None
+                    }
+                }
+            )
+        }
 
-        let (rest, map) = parse_bencode(torrent).unwrap();
-        assert!(rest.is_empty());
-        assert_eq!(
-            map,
-            BencodeValue::Dictionary(BDictionary {
-                dict: HashMap::from([
-                    (
-                        BString { content: b"foo" },
-                        BencodeValue::String(BString { content: b"bar" })
-                    ),
-                    (
-                        BString { content: b"list" },
-                        BencodeValue::List(BList {
-                            items: [
-                                BencodeValue::String(BString { content: b"angela" }),
-                                BencodeValue::String(BString { content: b"james" })
-                            ]
+        #[test]
+        fn complex() {
+            let torrent = b"d8:announce11:example.com4:infod4:name9:blindspot12:piece lengthi20e6:pieces5:hello5:filesld6:lengthi10e4:pathl5:path15:path2eeeee";
+            let torrent = parse_torrent(torrent).unwrap();
+            assert_eq!(
+                torrent,
+                Torrent {
+                    announce: "example.com".to_string(),
+                    info: Info {
+                        name: "blindspot".to_string(),
+                        piece_length: 20,
+                        pieces: [104, 101, 108, 108, 111].to_vec(),
+                        length: None,
+                        files: Some(
+                            [FilesInfo {
+                                length: 10,
+                                path: ["path1".to_string(), "path2".to_string()].to_vec()
+                            }]
                             .to_vec()
-                        })
-                    )
-                ])
-            })
-        )
+                        )
+                    }
+                }
+            )
+        }
     }
 }
